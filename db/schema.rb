bfb5fcd10d5e085f693b8e621a3b3f9342c56d97
zelzel2315 <criselda.valderrama@gmail.com>
2014-09-19 18:50:12 -0700

    added full name to collection select

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919223543) do
+ActiveRecord::Schema.define(version: 20140920003422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,9 @@ ActiveRecord::Schema.define(version: 20140919223543) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "true_shade"
+    t.string   "brand"
+    t.string   "product"
+    t.string   "shade"
   end
 
 end
