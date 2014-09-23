2911d2fcfca3dc9f16ac6aadd7fbec4dfc933ae0
carolynthu <ctnguyen.cn@gmail.com>
2014-09-22 21:33:37 -0700

    DO NOT MERGE THIS ONE

--- db/migrate/20140923042633_add_name_to_true_shades.rb
+++ db/migrate/20140923042633_add_name_to_true_shades.rb
@@ -1,5 +1,5 @@
 class AddNameToTrueShades < ActiveRecord::Migration
   def change
-  	add_column :true_shades, :name, :string
+    add_column :true_shades, :name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923033901) do
+ActiveRecord::Schema.define(version: 20140923041931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,10 @@ ActiveRecord::Schema.define(version: 20140923033901) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "true_shade"
   end
 
   add_index "makeups", ["true_shade_id"], name: "index_makeups_on_true_shade_id", using: :btree
-
-  create_table "true_shades", force: true do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "user_makeups", force: true do |t|
     t.integer  "makeup_id"
